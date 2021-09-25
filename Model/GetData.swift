3be83c4bcd00171dff38833b7f74038608c57b18
Dmitry <dimkamail@gmail.com>
2021-09-25 19:46:06 +0500

    Added some code

---
+++ Model/GetData.swift
@@ -0,0 +1,132 @@
+//
+//  GetData.swift
+//  AllCountriesApp
+//
+//  Created by Дмитрий on 25.09.2021.
+//
+
+import UIKit
+
+var name = [String]()
+var topLevelDomain = [String]()
+var alpha2Code = [String]()
+var alpha3Code = [String]()
+var callingCodes = [String]()
+var capital = [String]()
+var altSpellings = [String]()
+var region = [String]()
+var continent = [Continent]()
+var population = [Int]()
+var latlng = [Double]()
+var demonym = [String]()
+var area = [Double]()
+var gini = [Double]()
+var timezones = [String]()
+var borders = [String]()
+var nativeName = [String]()
+var numericCode = [String]()
+var currencies = [Currency]()
+var languages = [Language]()
+var translations = [Translations]()
+var flags = [String]()
+var regionalBlocs = [RegionalBloc]()
+var cioc = [String]()
+var independent = [Bool]()
+
+struct CountryStruct {
+    var name: String
+    var topLevelDomain: String
+    var alpha2Code: String
+    var alpha3Code: String
+    var callingCodes: String
+    var capital: String
+    var altSpellings: String
+    var region: String
+    var continent: Continent
+    var population: Int
+    var latlng: Double
+    var demonym: String
+    var area: Double
+    var gini: Double
+    var timezones: String
+    var borders: String
+    var nativeName: String
+    var numericCode: String
+    var currencies: Currency
+    var languages: Language
+    var translations: Translations
+    var flags: String
+    var regionalBlocs: RegionalBloc
+    var cioc: String
+    var independent: Bool
+}
+
+
+
+
+func getData() {
+    let urlString = "https://restcountries.com/v2/all"
+    let configuration = URLSessionConfiguration.ephemeral
+    let session = URLSession(configuration: configuration)
+    guard let url = URL(string: urlString) else {
+        print("Жопа")
+        return
+    }
+    let task = session.dataTask(with: url) { (data, response, error) in
+        guard let data = data else { return }
+        guard error == nil else { return }
+        do {
+            let countryInfo = try JSONDecoder().decode([CountryElement].self, from: data)
+            
+            for index in 0..<countryInfo.count {
+                name.append(countryInfo[index].name)
+                topLevelDomain.append(contentsOf: countryInfo[index].topLevelDomain)
+                alpha2Code.append(countryInfo[index].alpha2Code)
+                alpha3Code.append(countryInfo[index].alpha3Code)
+                callingCodes.append(contentsOf: countryInfo[index].callingCodes)
+                capital.append(countryInfo[index].capital ?? "")
+                altSpellings.append(contentsOf: countryInfo[index].altSpellings ?? [""])
+                region.append(countryInfo[index].region)
+                continent.append(countryInfo[index].continent)
+                population.append(countryInfo[index].population)
+                latlng.append(contentsOf: countryInfo[index].latlng ?? [0])
+                demonym.append(countryInfo[index].demonym ?? "")
+                area.append(countryInfo[index].area ?? 0)
+                gini.append(countryInfo[index].gini ?? 0)
+                timezones.append(contentsOf: countryInfo[index].timezones)
+                borders.append(contentsOf: countryInfo[index].borders ?? [""])
+                nativeName.append(countryInfo[index].nativeName)
+                numericCode.append(countryInfo[index].numericCode)
+                currencies.append(contentsOf: countryInfo[index].currencies ?? [Currency.init(code: "", name: "", symbol: "")])
+                languages.append(contentsOf: countryInfo[index].languages)
+                translations.append(countryInfo[index].translations)
+                flags.append(contentsOf: countryInfo[index].flags)
+                regionalBlocs.append(contentsOf: countryInfo[index].regionalBlocs ?? [RegionalBloc.init(acronym: Acronym.no, name: Name.no, otherNames: [OtherName.no], otherAcronyms: [OtherAcronym.no])])
+                cioc.append(countryInfo[index].cioc ?? "")
+                independent.append(countryInfo[index].independent)
+            }
+        } catch let error {
+            print(error)
+        }
+        print(languages[0].name)
+        print(languages[1].name)
+        print(languages[2].name)
+
+    }
+    task.resume()
+}
+
+func sortArrays() {
+    var sortedCountries = [CountryStruct]()
+    for index in 0..<name.count {
+        sortedCountries.append(CountryStruct(name: name[index], topLevelDomain: topLevelDomain[index], alpha2Code: alpha2Code[index], alpha3Code: alpha3Code[index], callingCodes: callingCodes[index], capital: capital[index], altSpellings: altSpellings[index], region: region[index], continent: continent[index], population: population[index], latlng: latlng[index], demonym: demonym[index], area: area[index], gini: gini[index], timezones: timezones[index], borders: borders[index], nativeName: nativeName[index], numericCode: numericCode[index], currencies: currencies[index], languages: languages[index], translations: translations[index], flags: flags[index], regionalBlocs: regionalBlocs[index], cioc: cioc[index], independent: independent[index]))
+    }
+//    sortedCountries = sortedCountries.sorted { item1, item2 in
+//        item1.name < item2.name
+//    }
+    print(sortedCountries[246].name)
+    print(sortedCountries[246].languages)
+    for index in 0..<sortedCountries.count {
+        print("\(sortedCountries[index].name) \(sortedCountries[index].capital) \(sortedCountries[index].population) \(sortedCountries[index].name)")
+    }
+}
---
+++ Model/JSON Model.swift
@@ -0,0 +1,211 @@
+//
+//  JSON Model.swift
+//  AllCountriesApp
+//
+//  Created by Дмитрий on 25.09.2021.
+//
+
+import UIKit
+
+struct CountryElement: Codable {
+    let name: String
+    let topLevelDomain: [String]
+    let alpha2Code: String
+    let alpha3Code: String
+    let callingCodes: [String]
+    let capital: String?
+    let altSpellings: [String]?
+    let region: String
+    let continent: Continent
+    let population: Int
+    let latlng: [Double]?
+    let demonym: String?
+    let area: Double?
+    let gini: Double?
+    let timezones: [String]
+    let borders: [String]?
+    let nativeName: String
+    let numericCode: String
+    let currencies: [Currency]?
+    let languages: [Language]
+    let translations: Translations
+    let flags: [String]
+    let regionalBlocs: [RegionalBloc]?
+    let cioc: String?
+    let independent: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case topLevelDomain = "topLevelDomain"
+        case alpha2Code = "alpha2Code"
+        case alpha3Code = "alpha3Code"
+        case callingCodes = "callingCodes"
+        case capital = "capital"
+        case altSpellings = "altSpellings"
+        case region = "region"
+        case continent = "continent"
+        case population = "population"
+        case latlng = "latlng"
+        case demonym = "demonym"
+        case area = "area"
+        case gini = "gini"
+        case timezones = "timezones"
+        case borders = "borders"
+        case nativeName = "nativeName"
+        case numericCode = "numericCode"
+        case currencies = "currencies"
+        case languages = "languages"
+        case translations = "translations"
+        case flags = "flags"
+        case regionalBlocs = "regionalBlocs"
+        case cioc = "cioc"
+        case independent = "independent"
+    }
+}
+
+enum Continent: String, Codable {
+    case africa = "Africa"
+    case americas = "Americas"
+    case antarctic = "Antarctic"
+    case antarcticOcean = "Antarctic Ocean"
+    case asia = "Asia"
+    case europe = "Europe"
+    case oceania = "Oceania"
+    case polar = "Polar"
+}
+
+struct Currency: Codable {
+    let code: String
+    let name: String
+    let symbol: String
+
+    enum CodingKeys: String, CodingKey {
+        case code = "code"
+        case name = "name"
+        case symbol = "symbol"
+    }
+}
+
+struct Language: Codable {
+    let iso6391: String?
+    let iso6392: String
+    let name: String
+    let nativeName: String?
+
+    enum CodingKeys: String, CodingKey {
+        case iso6391 = "iso639_1"
+        case iso6392 = "iso639_2"
+        case name = "name"
+        case nativeName = "nativeName"
+    }
+}
+
+struct RegionalBloc: Codable {
+    let acronym: Acronym
+    let name: Name
+    let otherNames: [OtherName]?
+    let otherAcronyms: [OtherAcronym]?
+
+    enum CodingKeys: String, CodingKey {
+        case acronym = "acronym"
+        case name = "name"
+        case otherNames = "otherNames"
+        case otherAcronyms = "otherAcronyms"
+    }
+}
+
+enum Acronym: String, Codable {
+    case al = "AL"
+    case asean = "ASEAN"
+    case au = "AU"
+    case cais = "CAIS"
+    case caricom = "CARICOM"
+    case cefta = "CEFTA"
+    case eeu = "EEU"
+    case efta = "EFTA"
+    case eu = "EU"
+    case nafta = "NAFTA"
+    case pa = "PA"
+    case saarc = "SAARC"
+    case usan = "USAN"
+    case no = ""
+}
+
+enum Name: String, Codable {
+    case africanUnion = "African Union"
+    case arabLeague = "Arab League"
+    case associationOfSoutheastAsianNations = "Association of Southeast Asian Nations"
+    case caribbeanCommunity = "Caribbean Community"
+    case centralAmericanIntegrationSystem = "Central American Integration System"
+    case centralEuropeanFreeTradeAgreement = "Central European Free Trade Agreement"
+    case eurasianEconomicUnion = "Eurasian Economic Union"
+    case europeanFreeTradeAssociation = "European Free Trade Association"
+    case europeanUnion = "European Union"
+    case northAmericanFreeTradeAgreement = "North American Free Trade Agreement"
+    case pacificAlliance = "Pacific Alliance"
+    case southAsianAssociationForRegionalCooperation = "South Asian Association for Regional Cooperation"
+    case unionOfSouthAmericanNations = "Union of South American Nations"
+    case no = ""
+}
+
+enum OtherAcronym: String, Codable {
+    case eaeu = "EAEU"
+    case sica = "SICA"
+    case unasul = "UNASUL"
+    case unasur = "UNASUR"
+    case uzan = "UZAN"
+    case no = ""
+}
+
+enum OtherName: String, Codable {
+    case accordDeLibreÉchangeNordAméricain = "Accord de Libre-échange Nord-Américain"
+    case alianzaDelPacífico = "Alianza del Pacífico"
+    case caribischeGemeenschap = "Caribische Gemeenschap"
+    case communautéCaribéenne = "Communauté Caribéenne"
+    case comunidadDelCaribe = "Comunidad del Caribe"
+    case jāmiAtAdDuwalAlArabīyah = "Jāmiʻat ad-Duwal al-ʻArabīyah"
+    case leagueOfArabStates = "League of Arab States"
+    case sistemaDeLaIntegraciónCentroamericana = "Sistema de la Integración Centroamericana,"
+    case southAmericanUnion = "South American Union"
+    case tratadoDeLibreComercioDeAméricaDelNorte = "Tratado de Libre Comercio de América del Norte"
+    case umojaWaAfrika = "Umoja wa Afrika"
+    case unieVanZuidAmerikaanseNaties = "Unie van Zuid-Amerikaanse Naties"
+    case unionAfricaine = "Union africaine"
+    case uniãoAfricana = "União Africana"
+    case uniãoDeNaçõesSulAmericanas = "União de Nações Sul-Americanas"
+    case uniónAfricana = "Unión Africana"
+    case uniónDeNacionesSuramericanas = "Unión de Naciones Suramericanas"
+    case الاتحادالأفريقي = "الاتحاد الأفريقي"
+    case جامعةالدولالعربية = "جامعة الدول العربية"
+    case no = ""
+}
+
+struct Translations: Codable {
+    let br: String
+    let pt: String
+    let nl: String?
+    let hr: String?
+    let fa: String?
+    let de: String?
+    let es: String?
+    let fr: String
+    let ja: String?
+    let it: String?
+    let hu: String?
+
+    enum CodingKeys: String, CodingKey {
+        case br = "br"
+        case pt = "pt"
+        case nl = "nl"
+        case hr = "hr"
+        case fa = "fa"
+        case de = "de"
+        case es = "es"
+        case fr = "fr"
+        case ja = "ja"
+        case it = "it"
+        case hu = "hu"
+    }
+}
+
+typealias Country = [CountryElement]
